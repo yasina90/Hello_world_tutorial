f7740d891fbd1ac76fc2d849d07d08880a46ba61
sinax <haleysina@gmail.com>
2024-05-14 16:12:11 +0330

    Signed-off-by: sinax <haleysina@gmail.com>

--- git_python.py
+++ git_python.py
@@ -29,6 +29,41 @@ Returns:
 precision_score(y_true=,y_pred=)
 
 
+
+import matplotlib.pylab as linestyle_str:
+'solid'      # Same as (0, ()) or '-'
+'dotted'     # Same as (0, (1, 1)) or ':'
+'dashed'     # Same as '--'
+'dashdot'    # Same as '-.'
+
+linestyle_tuple:
+'loosely dotted'        (0, (1, 10))
+'dotted'                (0, (1, 1))
+'densely dotted'        (0, (1, 1))
+
+'loosely dashed'        (0, (5, 10))
+'dashed'                (0, (5, 5))
+'densely dashed'        (0, (5, 1))
+
+'loosely dashdotted'    (0, (3, 10, 1, 10))
+'dashdotted'            (0, (3, 5, 1, 5))
+'densely dashdotted'    (0, (3, 1, 1, 1))
+
+'dashdotdotted'         (0, (3, 5, 1, 5, 1, 5))
+'loosely dashdotdotted' (0, (3, 10, 1, 10, 1, 10))
+'densely dashdotdotted' (0, (3, 1, 1, 1, 1, 1))
+
+color:
+'b'  # blue
+'g'  # green
+'r'  # red
+'c'  # cyan
+'m'  # magenta
+'y'  # yellow
+
+'k'  # black
+'w'  # white
+
 # asdasdas
 
 
