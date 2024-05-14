ac2d06b1cf302d34ed9ce16309d6e09d82640294
sinax <haleysina@gmail.com>
2024-05-14 16:09:44 +0330

    Signed-off-by: sinax <haleysina@gmail.com>

--- git_python.py
+++ git_python.py
@@ -12,11 +12,31 @@ import seaborn as sns
 # asdkasdlkasjd
 
 asdasdsa
+from sklearn.metrics import precision_score
+
+"""
+Calculates the precision score between the true labels `y_true` and the predicted labels `y_pred`.
+
+The precision score is the ratio of true positives to the sum of true positives and false positives. It measures the accuracy of positive predictions.
+
+Args:
+    y_true (list or numpy.ndarray): The true labels.
+    y_pred (list or numpy.ndarray): The predicted labels.
+
+Returns:
+    float: The precision score.
+"""
+precision_score(y_true=,y_pred=)
 
 
 # asdasdas
 
 
+
+
+
+
+# zcddsafcsdsd
 
 
 
